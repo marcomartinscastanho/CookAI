a0d402371ec7b5019863236de6272f34a9142be1
Marco Castanho <marco.castanho@toptal.com>
2024-02-09 17:15:37 +0000

    toast

---
+++ app/providers.tsx
@@ -0,0 +1,17 @@
+import React, { FC, ReactNode } from "react";
+import { Toaster } from "react-hot-toast";
+
+interface Props {
+  children: ReactNode;
+}
+
+const Providers: FC<Props> = ({ children }) => {
+  return (
+    <>
+      <Toaster position="top-center" />
+      {children}
+    </>
+  );
+};
+
+export default Providers;
